8265b11bf1b6b7b71edb03fe9995bda35e0d5640
krushnakant-borude <krushnaborude9007@gmail.com>
2025-12-30 15:40:23 +0530

    structure step using Cookiecutter done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End MLOPs Project consists of tools like MLFLOW, DVC, Kubernetes, Prometheus and Grafana. Project Deployed on AWS Cloud',
+    author='Krushnakant_Borude',
+    license='MIT',
+)
